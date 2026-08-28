48c720f92debd22ce108bdbd46a21e1d38d57875
Komal <komu94592@gmail.com>
2026-08-28 18:42:51 +0530

    Time: 12 ms (91.74%) | Memory: 12.5 MB (67.05%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,5 +5,19 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+        for char in t:
+            if char not in count:
+               return False
+            count[char] -= 1  
+        for value in count.values():
+            if value != 0:
+                return False   
+        return True           
         
