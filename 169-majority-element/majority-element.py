fafe18fddf853f665ded88d0f92b0749308d72d3
Komal <komu94592@gmail.com>
2026-09-11 22:59:22 +0530

    Time: 11 ms (50.33%) | Memory: 13.7 MB (40.15%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,13 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        res = 0    #boyer moore majority vote algorithum
         count = 0
-        for n in nums :
+        element = None
+        for num in nums:
             if count == 0:
-                res = n
-            count += (1 if n == res else -1) 
-        return res       
+                element = num
+            if num == element:
+                count += 1
+            else:
+                count -= 1
+        return element            
+            
+    
 
             
 
