a939530451b0b301f51ed8c249040706f5881e35
Komal <komu94592@gmail.com>
2026-09-19 23:29:14 +0530

    Time: 25 ms (11.72%) | Memory: 12.4 MB (14.56%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -3,11 +3,10 @@ class Solution:
         sign = -1 if x < 0 else 1
         x = abs(x)
         rev = 0
-
-        while x:
-            rev = rev * 10 + x % 10
+        while x > 0:
+            digit = x % 10
             x //= 10
-
-        rev *= sign
-
-        return rev if -2**31 <= rev <= 2**31 - 1 else 0  
+            rev = rev * 10 + digit 
+        if rev > 2 ** 31 - 1:
+            return 0    
+        return sign * rev    
